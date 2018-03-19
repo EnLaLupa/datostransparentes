6f0a95db86d8cc399d306ef2caccfeab8cc22795
Luis Miguel Leon Valenzuela <luiscleverboy@gmail.com>
2018-03-19 15:05:44 -0300

    Initial commit

---
+++ WebBuscadorDirectores/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using WebBuscadorDirectores.Models;
+
+namespace WebBuscadorDirectores.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CmfChileEntities _context = new CmfChileEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public JsonResult Buscar(string origenData)
+        {
+            var lista= _context.ListadoBusqueda.Where(r => r.busqueda.Contains(origenData)).ToList();
+            return Json(lista, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Buscar2()
+        {
+            var lista = _context.ListadoBusqueda.Select(r=>r.busqueda).ToList();
+            return Json(lista, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Detalle(string origenData)
+        {
+            string texto = origenData.Split('-')[0].Trim();
+            Regex reg = new Regex("[0-9]");
+            List<ListadoDirectores> lista = new List<ListadoDirectores>();
+            if (reg.IsMatch(texto))
+            {
+                lista = _context.ListadoDirectores.Where(r => r.Rut.StartsWith(texto) || r.ListadoFiscalizados.Rut.StartsWith(texto)).OrderBy(r => r.Nombre).ToList();
+            }
+            else
+            {
+                lista = _context.ListadoDirectores.Where(r => r.Nombre.Contains(texto) || r.ListadoFiscalizados.Entidad.Contains(texto)).OrderBy(r => r.Nombre).ToList();
+            }
+
+            var aux = lista.Select(r => new
+            {
+                PNombre = r.Nombre,
+                PRut = r.Rut,
+                PCargo = r.Cargo,
+                FEntidad = r.ListadoFiscalizados.Entidad,
+                FRut = r.ListadoFiscalizados.Rut
+            });
+            return Json(aux, JsonRequestBehavior.AllowGet);
+        }
+
+        
+
+    }
+}
---
+++ WebBuscadorDirectores/obj/Release/Package/PackageTmp/Scripts/info.js
@@ -0,0 +1,138 @@
+﻿var countries = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.whitespace,
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: '../../Home/Buscar2'
+});
+
+
+
+// passing in `null` for the `options` arguments will result in the default
+// options being used
+
+
+
+$(document).ready(function () {
+
+    $('#prefetch .typeahead').typeahead(null, {
+        name: 'countries',
+        source: countries
+    });
+
+    //$('#input-Elegido').on('keyup', function () {
+    //    var valor = $('#input-Elegido').val();
+    //    if (valor.length < 10) {
+    //        $('#Nombres').empty();
+    //    }
+    //    else {
+    //        if (valor.length == 10) {
+    //            $.ajax({
+    //                cache: false,
+    //                async: false,
+    //                type: 'POST',
+    //                data: {
+    //                    origenData: valor
+    //                },
+    //                url: '../../Home/Buscar',
+    //                dataType: 'json',
+    //                success: function (data) {
+    //                    $.each(data, function (index, item) {
+    //                        $('#Nombres').append("<option value='" + item.busqueda + "'>" + item.Rut + "</option>");
+    //                    });
+    //                },
+    //                //error: function (msg) {
+    //                //    $('#btnagendar').attr('disabled', false);
+    //                //}
+    //            });
+    //        }
+    //    }
+    //})
+
+    $("#input-Elegido").keypress(function (e) {
+        if (e.which == 13) {
+            consultadetalle();
+        }
+    });
+
+    $(".tt-menu").click(function (e) {
+       consultadetalle();
+        
+    });
+
+    $(".tt-menu").keypress(function (e) {
+        if (e.which == 13) {
+            consultadetalle();
+        }
+    });
+});
+
+function consultadetalle() {
+    var valor = $('#input-Elegido').val();
+    $('#detalle').empty();
+    $.ajax({
+        cache: false,
+        async: false,
+        type: 'POST',
+        data: {
+            origenData: valor
+        },
+        url: '../../../Home/Detalle',
+        dataType: 'json',
+        success: function (data) {
+            var eLLL = data[0].PNombre;
+            var aLLL = true;
+            $.each(data, function (index, item) {
+                if (eLLL == item.PNombre) {
+                    if (aLLL) {
+                        $("#detalle").append('<div class="col-12 margin"><b> Rut: </b>' + item.PRut + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Nombre: </b>' + item.PNombre + '</div>')
+                        $("#detalle").append('<div class="col-4 borde">RUT</div><div class="col-4 borde">EMPRESA</div><div class="col-4 borde">CARGO</div>')
+                        $('#detalle').append(agregar(item));
+                        aLLL = false;
+                    }
+                    else {
+                        $('#detalle').append(agregar(item));
+                    }
+                }
+                else {
+                    eLLL = item.PNombre;
+                    aLLL = true;
+                    $("#detalle").append('<div class="col-12 separacion text-center">- - - - - - - - - - -  - -</div>')
+                    $("#detalle").append('<div class="col-12 margin"><b> Rut: </b>' + item.PRut + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Nombre: </b>' + item.PNombre + '</div>')
+                    $("#detalle").append('<div class="col-4 borde">RUT</div><div class="col-4 borde">EMPRESA</div><div class="col-4 borde">CARGO</div>')
+                    $('#detalle').append(agregar(item));
+                    aLLL = false;
+
+                }
+
+            });
+
+
+            //$("#detalle").append('<div class="col-12 margin"><b> Rut: </b>' + data[0].PRut + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Nombre: </b>' + data[0].PNombre + '</div>')
+            //$("#detalle").append('<div class="col-4 borde">RUT</div><div class="col-4 borde">EMPRESA</div><div class="col-4 borde">CARGO</div>')
+            //$.each(data, function (index, item) {
+            //    if (auxiliar) {
+
+            //    }
+            //    $('#detalle').append(agregar(item));
+            //});
+        },
+        //error: function (msg) {
+        //    $('#btnagendar').attr('disabled', false);
+        //}
+    });
+}
+
+function buscar() {
+    var valor = $('#input-Elegido').val();
+    if (valor.length > 0) {
+        consultadetalle();
+    }
+}
+
+
+function agregar(item) {
+    return '\
+            <div class="col-4">' + item.FRut + '</div>\
+            <div class="col-4">' + item.FEntidad + '</div>\
+            <div class="col-4">' + item.PCargo + '</div>\
+            '
+}
